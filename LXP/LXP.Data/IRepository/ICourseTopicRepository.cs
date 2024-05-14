f08ab4a5bc7453214fee594f59aeffa149420113
smano-20052002 <smano4570@gmail.com>
2024-05-14 15:40:49 +0530

    Initial

---
+++ LXP/LXP.Data/IRepository/ICourseTopicRepository.cs
@@ -0,0 +1,23 @@
+﻿using LXP.Common.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LXP.Data.IRepository
+{
+    public interface ICourseTopicRepository
+    {
+        object GetAllTopicDetailsByCourseId(string courseId);
+        void AddCourseTopic(Topic topic);
+        bool AnyTopicByTopicName(string topicName);
+        
+        Topic GetTopicDetailsByTopicNameAndCourse(string topicName, Course course);
+
+        Task<int> UpdateCourseTopic(Topic topic);
+        Task<Topic> GetTopicByTopicId(Guid topicId);
+
+
+    }
+}
